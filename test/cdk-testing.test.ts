0a4ba24981e6b09b4f48966fd46f79d09d325ae3
Benoit Durand <benoit.durand.mail@gmail.com>
2025-02-23 18:45:07 +0100

    fix tests

--- test/cdk-testing.test.ts
+++ test/cdk-testing.test.ts
@@ -1,17 +1,52 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as CdkTesting from '../lib/cdk-testing-stack';
+import { Capture, Match, Template } from "aws-cdk-lib/assertions";
+import * as cdk from "aws-cdk-lib";
+import * as sns from "aws-cdk-lib/aws-sns";
+import { StateMachineStack } from "../lib/state-machine-stack";
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/cdk-testing-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new CdkTesting.CdkTestingStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+describe("StateMachineStack", () => {
+  test("synthesizes the way we expect", () => {
+    const app = new cdk.App();
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+    // Since the StateMachineStack consumes resources from a separate stack
+    // (cross-stack references), we create a stack for our SNS topics to live
+    // in here. These topics can then be passed to the StateMachineStack later,
+    // creating a cross-stack reference.
+    const topicsStack = new cdk.Stack(app, "TopicsStack");
+
+    // Create the topic the stack we're testing will reference.
+    const topics = [new sns.Topic(topicsStack, "Topic1", {})];
+
+    // Create the StateMachineStack.
+    const stateMachineStack = new StateMachineStack(app, "StateMachineStack", {
+      topics: topics, // Cross-stack reference
+    });
+
+    // Prepare the stack for assertions.
+    const template = Template.fromStack(stateMachineStack);
+
+    // Assert it creates the function with the correct properties...
+    template.hasResourceProperties("AWS::Lambda::Function", {
+      Handler: "handler",
+      Runtime: "nodejs18.x",
+    });
+
+    // Fully assert on the state machine's IAM role with matchers.
+    template.hasResourceProperties(
+      "AWS::IAM::Role",
+      Match.objectEquals({
+        AssumeRolePolicyDocument: {
+          Version: "2012-10-17",
+          Statement: [
+            {
+              Action: "sts:AssumeRole",
+              Effect: "Allow",
+              Principal: {
+                Service: Match.stringLikeRegexp("\\S+.amazonaws.com")
+              },
+            },
+          ],
+        },
+      })
+    );
+  });
 });
