a3ca8b6f58fb44235499035e4bc28da5aae525db
Benoit Durand <benoit.durand.mail@gmail.com>
2025-02-23 17:57:20 +0100

    add stack

---
+++ bin/state-machine.ts
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+import * as cdk from "aws-cdk-lib";
+
+const app = new cdk.App();
+
+// Stacks are intentionally not created here -- this application isn't meant to
+// be deployed.
